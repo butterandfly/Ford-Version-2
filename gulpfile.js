6268f47f6cbd46616c44a1baefbb35e6d0cea920
butterandfly <zero.hero.lin@gmail.com>
2015-01-15 18:17:57 +0800

    更新了表单样式，添加了ip统计

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
-var sassFile = ["./css/main.scss", './css/pc_main.scss'];
+var sassFiles = ["./css/main.scss", './css/pc_main.scss'];
+var jsFiles = ['./js/pc/*.js'];
+
+var targetCssDir = './css/';
+var targetJsDir = './js/';
 
 gulp.task('sass', function() {
-  gulp.src(sassFile)
+  gulp.src(sassFiles)
     .pipe(sass())
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest(targetCssDir));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch([sassFile], ['sass']);
+gulp.task('concat-js-pc', function() {
+  gulp.src(jsFiles)
+    .pipe(concat('main_pc.js'))
+    .pipe(gulp.dest(targetJsDir));
+})
+
+gulp.task('default', ['sass', 'concat-js-pc'], function() {
+  gulp.watch([sassFiles], ['sass']);
+  gulp.watch([jsFiles], ['concat-js-pc']);
 });
