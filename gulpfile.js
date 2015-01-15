dc53809ad976578fcdaf21558fe78f24569b4603
butterandfly <zero.hero.lin@gmail.com>
2015-01-15 11:16:05 +0800

    添加了pc页index_pc.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-var sassFile = "./css/main.scss";
+var sassFile = ["./css/main.scss", './css/pc_main.scss'];
 
 gulp.task('sass', function() {
   gulp.src(sassFile)
