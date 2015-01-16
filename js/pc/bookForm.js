1282dd1d37ba2d7e27a7bb084d526c472879c0a2
butterandfly <zero.hero.lin@gmail.com>
2015-01-16 16:56:20 +0800

    更新了表单，添加了活动详情

--- js/pc/bookForm.js
+++ js/pc/bookForm.js
@@ -79,36 +79,3 @@
     });
   });
 })();
-(function () {
-  "use strict";
-
-  var floatFooter = $('.float-footer');
-  var topBookBtn = $('.top-book-btn');
-  var bottomBookBtn = $('.bottom-book-btn');
-
-
-  // 隐藏下方浮层
-  floatFooter.hide();
-
-  // 滚动过header的时候现实下方浮层
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 560) {
-      floatFooter.show();
-    } else {
-      floatFooter.hide();
-    }
-  });
-
-// 预约按钮（上）
-  topBookBtn.on('click', function () {
-    ___log('打开预约表单（上）');
-  });
-// 预约按钮（下）
-  bottomBookBtn.on('click', function () {
-    ___log('打开预约表单（下）');
-  });
-
-})();
-
-
-
